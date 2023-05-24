8538ad97bbac86e75169b971a0d52a8169d53516
codelover7771 <umeshramyadav0022@gmail.com>
2023-05-25 01:44:14 +0530

    minor update

--- poc-project/src/components/TableData.jsx
+++ poc-project/src/components/TableData.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function DataTable({ data }) {
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handleCheckboxChange = (index) => {
+    if (selectedRows.includes(index)) {
+      setSelectedRows(selectedRows.filter((rowIndex) => rowIndex !== index));
+    } else {
+      setSelectedRows([...selectedRows, index]);
+    }
+  };
+
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <table className="table">
         <thead>
           <tr>
             <th className="p-3 text-center">Activities</th>
-            <th className="p-3 text-center"> Accountable</th>
+            <th className="p-3 text-center">Accountable</th>
             <th className="p-3 text-center">Artifact</th>
             <th className="p-3 text-center">Due Date</th>
             <th className="p-3 text-center">Status</th>
@@ -16,7 +26,14 @@ export default function DataTable({ data }) {
         <tbody>
           {data.accountable.map((accountable, index) => (
             <tr key={index}>
-              <td className="p-3 text-center nowrap">{data.activities[index] }</td>
+              <td className="p-3 text-center nowrap">
+                <input
+                  type="checkbox"
+                  checked={selectedRows.includes(index)}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+                {data.activities[index]}
+              </td>
               <td className="p-3 text-center nowrap">{accountable}</td>
               <td className="p-3 text-center nowrap">{data.artifact[index]}</td>
               <td className="p-3 text-center nowrap">{data.due_date[index]}</td>
